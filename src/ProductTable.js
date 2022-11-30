306cfda9e03faa3adaf1e54fd6bbbdbf9ce2179b
HyungKu Im <damansa1@naver.com>
2022-11-30 15:01:36 +0900

    중간 소스

---
+++ src/ProductTable.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class ProductTable extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: [
+                {
+                    name: "Samsung S23 Plus",
+                    type: "Mobile",
+                    description: "이번엔 제대로 나와 줬으면...",
+                    brand: "Samsung"
+                },
+                {
+                    name: "iPhone 15",
+                    type: "Mobile",
+                    description: "안접혀도 예쁨",
+                    brand: "Apple"
+                },
+                {
+                    name: "Dimche",
+                    type: "Kimchi 냉장고",
+                    description: "김치는 딤채",
+                    brand: "Dimche"
+                }
+            ]
+        }
+
+        
+    }
+
+    render() {
+        return (
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Type</th>
+                        <th>Brand</th>
+                        <th>description</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                        <th>View</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.products.map((detail,i)
+                        => <ProductRow key={i} product={detail})}
+                </tbody>
+            </table>
+        );
+    }
+}
+
+export default ProductTable;
